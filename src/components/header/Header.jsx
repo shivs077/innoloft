d1395241b909aa8c22972b2917293b63f81e50ea
Shivam <shivam.bisht@thoughts2binary.com>
2023-08-13 14:32:29 +0530

    added heroicons lib, header & sidebar added layout

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,47 @@
+// lib
+import { MagnifyingGlassIcon, BellIcon, ChatBubbleOvalLeftEllipsisIcon, ChevronDownIcon, Bars3Icon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
+
+// components
+import logo from "../../assets/logo.svg";
+import userImage from "../../assets/user_default.jpg";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex bg-primary p-4">
+      <div className="flex items-center w-full mx-auto max-w-[1280px] relative">
+        <div className="md:w-[200px] lg:w-[280px]">
+          <Link to="/">
+            <img src={logo} className="object-contain w-[140px]" />
+          </Link>
+        </div>
+        <div className="flex justify-end md:justify-between flex-1">
+          <div className="hidden md:flex bg-white px-2 rounded-[4px] md:w-[300px] lg:w-[450px] justify-between">
+            <input
+              placeholder="Enter interests, keywords, company name, etc."
+              className="outline-none border-none text-[12px] text-[#374151] flex-1 h-[28px] placeholder-[#374151]"
+            />
+            <MagnifyingGlassIcon className="w-[20px] stroke-[#374151] rotate-90" />
+          </div>
+          <div className="flex gap-3 items-center text-white">
+            <ChatBubbleOvalLeftEllipsisIcon className="hidden md:flex w-[22px]" />
+            <div className="hidden md:flex items-center gap-1">
+              <p>EN</p>
+              <ChevronDownIcon className="w-[24px]" />
+            </div>
+            <BellIcon className="hidden md:flex w-[22px]" />
+            <div className="hidden md:flex items-center gap-1">
+              <img className="h-6 w-6 rounded-full" src={userImage} alt="User Profile" />
+              <ChevronDownIcon className="w-[24px]" />
+            </div>
+            <div className=" flex lg:hidden">
+              <Bars3Icon className="w-[24px] " />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,36 @@
+import { HomeIcon, ShoppingBagIcon } from "@heroicons/react/24/outline";
+import userImage from "../../assets/user_default.jpg";
+import { Link } from "react-router-dom";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="hidden lg:flex flex-col lg:w-[256px] py-8 px-2">
+      {/* User Profile */}
+      <div className="flex items-center mb-4">
+        <img className="w-12 h-12 lg:h-16 lg:w-16 rounded-full mr-4" src={userImage} alt="User Profile" />
+        <div>
+          <p className="text-lg font-medium">Sven Pietsch</p>
+          <p className="text-[14px] font-light">Innoloft GmbH</p>
+        </div>
+      </div>
+
+      {/* Navigation */}
+      <ul className="space-y-3">
+        <li className="flex items-end gap-3 py-2 hover:bg-slate-50">
+          <HomeIcon className="w-[20px]" />
+          <Link to={"/"} className="flex flex-1">
+            <p className="font-light text-[17px] leading-[17px]">Home</p>
+          </Link>
+        </li>
+        <li className="flex items-end gap-3 py-2 hover:bg-slate-50">
+          <ShoppingBagIcon className="w-[20px]" />
+          <Link to={"/product"} className="flex flex-1">
+            <p className="font-light text-[17px] leading-[17px]">Product</p>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
